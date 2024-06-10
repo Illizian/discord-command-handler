595c3d8e699f8ff7843853a7e27279adb3e92e75
Alex Scotton <alex@alexscotton.com>
2024-06-10 10:20:22 +0100

    A Laravel emerges

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Discord Hub</title>
+</head>
+
+<body>
+    <h1>Discord Hub</h1>
+</body>
+
+</html>
