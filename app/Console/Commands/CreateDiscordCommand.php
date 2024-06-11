4117e0b60b027ffb9a0cd4935d8eb271e97f7eff
Alex Scotton <alex@alexscotton.com>
2024-06-11 19:52:24 +0100

    feat: move command creation to a command, and update to use subgroups for me/top & mark

---
+++ app/Console/Commands/CreateDiscordCommand.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class CreateDiscordCommand extends Command
+{
+    protected $signature = 'app:create-discord-command';
+
+    protected $description = 'Pushes the command to Discord';
+
+    const CONFIG = [
+        'name' => 'answered',
+        'description' => 'Basically stackoverflow...',
+        'options' => [
+            [
+                'name' => 'mark',
+                'description' => 'Mark this thread as answered.',
+                'type' => 1,
+                'options' => [
+                    [
+                        'name' => 'by',
+                        'description' => 'Who helped?',
+                        'type' => 6,
+                        'required' => true,
+                    ],
+                ],
+            ],
+            [
+                'name' => 'top',
+                'description' => 'See Top 10 Answerers.',
+                'type' => 1,
+            ],
+            [
+                'name' => 'me',
+                'description' => 'See your answers.',
+                'type' => 1,
+            ],
+        ],
+    ];
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): int
+    {
+        $token = config('services.discord.api_token');
+        $appId = config('services.discord.client_id');
+
+        $response = Http::acceptJson()
+            ->withHeaders([
+                'Authorization' => "Bot $token",
+            ])
+            ->post(
+                // @TODO: Using guild id here, before we apply command globally.
+                "https://discord.com/api/v10/applications/$appId/guilds/331884122010025986/commands",
+                self::CONFIG
+            );
+
+        dump($response->json());
+
+        return 0;
+    }
+}
